d1bcb570ce1d08244384a47e2406a6648bebaa15
Lucas Ramos <lucas.nogueira.gft@youse.com.br>
2022-07-21 08:23:08 -0300

    feat: create properties file and loading url from it
    added to gitignore in order to avoid commiting private data

--- stickers/src/App.java
+++ stickers/src/App.java
@@ -1,3 +1,4 @@
+import java.io.FileInputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -5,10 +6,16 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class App {
+    static String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+    static String appConfigPath = rootPath + "application.properties";
+    protected static Properties applicationProperties = new Properties();
+
     public static void main(String[] args) throws Exception {
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
+        applicationProperties.load(new FileInputStream(appConfigPath));
+        String url = applicationProperties.getProperty("API_URL");
         URI uriAddress = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(uriAddress).GET().build();
@@ -19,8 +26,8 @@ public class App {
         var parser = new JsonParser();
         List<Map<String, String>> movieList = parser.parse(body);
 
-        // exibir e manipular os dados 
-        for (Map<String,String> filme : movieList) {
+        // exibir e manipular os dados
+        for (Map<String, String> filme : movieList) {
             System.out.println(filme.get("title"));
             System.out.println(filme.get("image"));
             System.out.println(filme.get("imDbRating"));
