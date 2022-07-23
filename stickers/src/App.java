942b7775c4a25b510a8aaa8d9ef79c342f1c09e7
Lucas Nogueira Ramos <lucasnramos@gmail.com>
2022-07-23 00:22:29 -0300

    Class 3 (#2)
    * feat: Content class
    
    * feat: create API client class
    
    * Refactor App to use API Client
    
    * feat: extractor interface
    
    Co-authored-by: Lucas Ramos <lucas.nogueira.gft@youse.com.br>

--- stickers/src/App.java
+++ stickers/src/App.java
@@ -18,29 +18,14 @@ public class App {
     public static void main(String[] args) throws Exception {
         applicationProperties.load(new FileInputStream(appConfigPath));
         String url = applicationProperties.getProperty("API_URL");
-        URI uriAddress = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(uriAddress).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-
-        // extrair só os dados que interessam (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> movieList = parser.parse(body);
-
-        // exibir e manipular os dados
-        for (Map<String, String> movie : movieList) {
-            System.out.println(movie.get("title"));
-            System.out.println(movie.get("image"));
-            System.out.println(movie.get("imDbRating"));
-            System.out.println();
-        }
-
-        // Aula 2 - gerando imagem para sticker
+        String jsonContent = new ApiClient().fetch(url);
+        ContentExtractor nasaExtractor = new NasaContentExtractor();
+        // ContentExtractor movieExtractor = new NasaContentExtractor();
+        List<Content> contentList = nasaExtractor.extract(jsonContent);
         var factory = new StickerFactory();
-        for (Map<String, String> filme : movieList) {
-            String imgUrl = filme.get("image");
-            String title = filme.get("title");
+        for (Content content : contentList) {
+            String imgUrl = content.getImageUrl();
+            String title = content.getTitle();
             InputStream inputStream = new URL(imgUrl).openStream();
             String fileName = title + ".png";
             factory.create(inputStream, fileName);
