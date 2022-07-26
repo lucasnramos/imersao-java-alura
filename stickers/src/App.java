1337007460e310270446d6113236f135fe1a9f29
Lucas Ramos <lucas.nogueira.gft@youse.com.br>
2022-07-26 17:54:43 -0300

    feat: replaced custom json parser with Jackson

--- stickers/src/App.java
+++ stickers/src/App.java
@@ -1,20 +1,20 @@
 import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App {
     private static Properties properties = new Properties();
 
     public static void main(String[] args) throws Exception {
+        // read API_URL from .properties file
         properties.load(new FileInputStream("application.properties"));
         String url = properties.getProperty("API_URL");
 
@@ -24,13 +24,18 @@ public class App {
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
 
-        var parser = new JsonParser();
-        List<Map<String, String>> movieList = parser.parse(body);
+        // Parse response string as json using Jackson
+        ObjectMapper jsonMapper = new ObjectMapper();
+        var readValue = jsonMapper.readValue(body, ImdbJsonResponse.class);
+        var movieList = readValue.items;
 
+        // Iterate the movieList to print each movie's title, img and rating
         for (Map<String, String> filme : movieList) {
             var rating = getRating(filme.get("imDbRating"));
-            System.out.println(AnsiColors.BLUE + "Titulo do Filme: " + filme.get("title"));
-            System.out.println(AnsiColors.PURPLE + "Link do Poster: " + filme.get("image"));
+            System.out.println(AnsiColors.BLUE + "Titulo do Filme: " +
+                    filme.get("title"));
+            System.out.println(AnsiColors.PURPLE + "Link do Poster: " +
+                    filme.get("image"));
             System.out.println(AnsiColors.RED + "Score: " + rating);
             System.out.println();
         }
