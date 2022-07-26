6c4eb6979d644ec85d18916a28c05b3a23da7db6
Lucas Ramos <lucas.nogueira.gft@youse.com.br>
2022-07-26 13:55:22 -0300

    feat: first class doc and changes to API URL

--- stickers/src/App.java
+++ stickers/src/App.java
@@ -1,4 +1,3 @@
-import java.io.FileInputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -6,32 +5,39 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 public class App {
-    static String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-    static String appConfigPath = rootPath + "application.properties";
-    protected static Properties applicationProperties = new Properties();
+    static private final String apiUrl = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
 
     public static void main(String[] args) throws Exception {
-        applicationProperties.load(new FileInputStream(appConfigPath));
-        String url = applicationProperties.getProperty("API_URL");
+        // read API_URL from properties file
+        String url = apiUrl;
+        // Creates new URI object to build a new HTTP Client and request
         URI uriAddress = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(uriAddress).GET().build();
+        // Sends a get request to the API and retrieves body
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
 
-        // extrair só os dados que interessam (titulo, poster, classificação)
         var parser = new JsonParser();
         List<Map<String, String>> movieList = parser.parse(body);
 
-        // exibir e manipular os dados
-        for (Map<String, String> filme : movieList) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+        // reading only first 3 for easy debug
+        for (int i = 0; i < 3; i++) {
+            var filme = movieList.get(i);
+            System.out.println("Titulo do Filme: " + filme.get("title"));
+            System.out.println("Link do Poster: " + filme.get("image"));
+            System.out.println("Score: " + filme.get("imDbRating"));
             System.out.println();
         }
+
+        // prints all movies
+        // for (Map<String, String> filme : movieList) {
+        // System.out.println("Titulo do Filme: " + filme.get("title"));
+        // System.out.println("Link do Poster: " + filme.get("image"));
+        // System.out.println("Score: " + filme.get("imDbRating"));
+        // System.out.println();
+        // }
     }
 }
