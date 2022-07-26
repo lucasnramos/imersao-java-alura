f6b75dd809d3193e442160b4fe5406a4601c15ef
Lucas Nogueira Ramos <lucasnramos@gmail.com>
2022-07-26 18:01:44 -0300

    Desafios da aula 1 (#4)
    * feat: update from master
    
    * fix: Readme format
    
    * feat: first class doc and changes to API URL
    
    * feat: using ansi colors for println
    
    Also using emojis for ratings on movies
    
    * feat: move api url to .properties file
    
    a file called `application.properties` should be placed  at the root
    of the project.
    
    * feat: replaced custom json parser with Jackson
    
    Co-authored-by: Lucas Ramos <lucas.nogueira.gft@youse.com.br>

---
+++ docs/aula_1.md
@@ -0,0 +1,7 @@
+# Imersão Java - Aula 1
+
+Construção de aplicação Java sem `libs` externas para consumir uma API Pública de filmes e apresentar no console o nome, link do poster e pontuação.
+
+## IMDb Fora do ar - alternativas
+
+o IMDb ficou fora do ar e indisponível durante a imersão, como alternativas, podemos usar o [ Open Movie Database ](https://www.omdbapi.com/) ou bases abertas criadas pela comunidade.
--- stickers/src/App.java
+++ stickers/src/App.java
@@ -10,27 +10,55 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class App {
-    static String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-    static String appConfigPath = rootPath + "application.properties";
-    protected static Properties applicationProperties = new Properties();
+    private static Properties properties = new Properties();
 
     public static void main(String[] args) throws Exception {
-        applicationProperties.load(new FileInputStream(appConfigPath));
-        String url = applicationProperties.getProperty("API_URL");
-        String jsonContent = new ApiClient().fetch(url);
-        ContentExtractor nasaExtractor = new NasaContentExtractor();
-        // ContentExtractor movieExtractor = new NasaContentExtractor();
-        List<Content> contentList = nasaExtractor.extract(jsonContent);
-        var factory = new StickerFactory();
-        for (Content content : contentList) {
-            String imgUrl = content.getImageUrl();
-            String title = content.getTitle();
-            InputStream inputStream = new URL(imgUrl).openStream();
-            String fileName = title + ".png";
-            factory.create(inputStream, fileName);
-            System.out.println(title);
+        // read API_URL from .properties file
+        properties.load(new FileInputStream("application.properties"));
+        String url = properties.getProperty("API_URL");
+
+        URI uriAddress = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(uriAddress).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // Parse response string as json using Jackson
+        ObjectMapper jsonMapper = new ObjectMapper();
+        var readValue = jsonMapper.readValue(body, ImdbJsonResponse.class);
+        var movieList = readValue.items;
+
+        // Iterate the movieList to print each movie's title, img and rating
+        for (Map<String, String> filme : movieList) {
+            var rating = getRating(filme.get("imDbRating"));
+            System.out.println(AnsiColors.BLUE + "Titulo do Filme: " +
+                    filme.get("title"));
+            System.out.println(AnsiColors.PURPLE + "Link do Poster: " +
+                    filme.get("image"));
+            System.out.println(AnsiColors.RED + "Score: " + rating);
             System.out.println();
         }
+    }
+
+    private static String getRating(String rating) {
+        String halfStep = " 💔 ";
+        String fullStep = " ❤️ ";
+        String emojiRating = "";
+        Float numberRating = Float.parseFloat(rating);
+        Boolean ratingHasDecimal = numberRating % 2 != 0;
+        Integer intRating = numberRating.intValue();
+
+        for (int i = 0; i < intRating; i++) {
+            emojiRating = emojiRating + fullStep;
+        }
+
+        if (ratingHasDecimal) {
+            emojiRating = emojiRating + halfStep;
+        }
+
+        return emojiRating;
     }
 }
