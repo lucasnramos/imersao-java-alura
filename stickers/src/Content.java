de1beeef489f933b29402109c30ae8cfd58d6d3e
Lucas Ramos <lucas.nogueira.gft@youse.com.br>
2022-07-26 23:40:55 -0300

    feat: Content from class to record

--- stickers/src/Content.java
+++ stickers/src/Content.java
@@ -1,17 +1,2 @@
-public class Content {
-	private final String title;
-	private final String imageUrl;
-
-	public Content(String title, String imageUrl) {
-		this.title = title;
-		this.imageUrl = imageUrl;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
+public record Content(String title, String imageUrl) {
 }
